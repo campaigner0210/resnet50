b122fc06b13c3074a5d222c42b2a731c77d5e656
campaigner0210 <86864592+campaigner0210@users.noreply.github.com>
2022-06-26 17:41:34 +0800

    Update infer.py

--- infer.py
+++ infer.py
@@ -8,9 +8,6 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-# %matplotlib inline
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-
 
 def softmax(x):
     exp_x = np.exp(x)
@@ -26,17 +23,17 @@ if __name__ == "__main__":
     test_list = 'test.txt'
     test_data = Garbage_Loader(test_list, train_flag=False)
     test_loader = DataLoader(dataset=test_data, num_workers=1, pin_memory=True, batch_size=1)
-    model = models.resnet50(pretrained=False)
+    model = models.resnet101(pretrained=False)
     fc_inputs = model.fc.in_features
     model.fc = nn.Linear(fc_inputs, 214)
     model = model.cuda()
     # 加载训练好的模型
-    checkpoint = torch.load('model_best_checkpoint_resnet50.pth.tar')
+    checkpoint = torch.load('model/model_best/best_checkpoint_resnet101.pth.tar')
     model.load_state_dict(checkpoint['state_dict'])
     model.eval()
     for i, (image, label) in enumerate(test_loader):
         src = image.numpy()
-        src = src.reshape(3, 224, 224)
+        src = src.reshape(3, 160, 160)
         src = np.transpose(src, (1, 2, 0))
         image = image.cuda()
         label = label.cuda()
